78ce0fe0a3d5e2241978f73cf70f672d79f51612
Adam McDaniel <adam.mcdaniel17@gmail.com>
2021-10-13 12:05:46 -0400

    Added width and height methods for console module

--- src/binary/init/console_module.rs
+++ src/binary/init/console_module.rs
@@ -1,13 +1,30 @@
 use common_macros::b_tree_map;
-use dune::{Environment, Error, Expression};
+use dune::{Int, Environment, Error, Expression};
+use terminal_size::{terminal_size, Width, Height};
 
 pub fn get() -> Expression {
     (b_tree_map! {
+        String::from("width") => Expression::builtin("width", width, "get the width of the console"),
+        String::from("height") => Expression::builtin("height", height, "get the height of the console"),
         String::from("write") => Expression::builtin("write", write, "write text to a specific position in the console"),
         String::from("title") => Expression::builtin("title", title, "set the title of the console"),
         String::from("clear") => Expression::builtin("clear", clear, "clear the console"),
     })
     .into()
+}
+
+fn width(_: Vec<Expression>, _: &mut Environment) -> Result<Expression, Error> {
+    Ok(match terminal_size() {
+        Some((Width(w), _)) => (w as Int).into(),
+        _ => Expression::None,
+    })
+}
+
+fn height(_: Vec<Expression>, _: &mut Environment) -> Result<Expression, Error> {
+    Ok(match terminal_size() {
+        Some((_, Height(h))) => (h as Int).into(),
+        _ => Expression::None,
+    })
 }
 
 fn write(args: Vec<Expression>, env: &mut Environment) -> Result<Expression, Error> {
